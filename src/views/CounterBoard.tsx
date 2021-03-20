85a9f56d62eee8c6d2af1384e55aea48ae2ccd1a
motoroller.scalatron <motoroller.scalatron75@gmail.com>
2021-03-20 12:09:48 +0900

    [WIP] add buttons

--- src/views/CounterBoard.tsx
+++ src/views/CounterBoard.tsx
@@ -1,6 +1,6 @@
 // import "../../tailwind.output.css"
 
-import React, { FC, useState, useCallback } from "react";
+import React, { FC, useState, useCallback, useEffect } from "react";
 import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
 import "../assets/tailwind.output.css";
@@ -16,6 +16,13 @@ const CounterBoard: FC<{ initial?: number }> = ({
   const [count4, setCount4] = useState(initial);
   const [count5, setCount5] = useState(initial);
   const [count6, setCount6] = useState(initial);
+
+  const [countPoolLeft, setCountPoolLeft] = useState(pool);
+  useEffect(() => {
+    setCountPoolLeft(
+      pool - (count1 + count2 + count3 + count4 + count5 + count6)
+    );
+  }, [count1, count2, count3, count4, count5, count6]);
 
   const Attrib = {
     str: "Strength",
@@ -75,36 +82,50 @@ const CounterBoard: FC<{ initial?: number }> = ({
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <Header />
-      <h1>Board</h1>
-      <div>
+      <h1>Charname</h1>
+      <div></div>
+      <div className="border-2">
         <p> Str {count1}</p>
         <button onClick={() => handleAmount("str", true)}>+</button>
         <button onClick={() => handleAmount("str", false)}>-</button>
       </div>
-      <div>
+      <div className="border-2">
         <p> Dex {count2}</p>
         <button onClick={() => handleAmount("dex", true)}>+</button>
         <button onClick={() => handleAmount("dex", false)}>-</button>
       </div>
-      <div>
+      <div className="border-2">
         <p> Con {count3}</p>
         <button onClick={() => handleAmount("con", true)}>+</button>
         <button onClick={() => handleAmount("con", false)}>-</button>
       </div>
-      <div>
+      <div className="border-2">
         <p> Int {count4}</p>
         <button onClick={() => handleAmount("int", true)}>+</button>
         <button onClick={() => handleAmount("int", false)}>-</button>
       </div>
-      <div>
+      <div className="border-2">
         <p> Wis {count5}</p>
         <button onClick={() => handleAmount("wis", true)}>+</button>
         <button onClick={() => handleAmount("wis", false)}>-</button>
       </div>
-      <div>
+      <div className="border-2">
         <p> Cha {count6}</p>
         <button onClick={() => handleAmount("cha", true)}>+</button>
         <button onClick={() => handleAmount("cha", false)}>-</button>
+      </div>
+      <div className="border-2">
+        <p>Attr points left: {countPoolLeft}</p>
+      </div>
+      <div className="my-2">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Store
+        </button>
+      </div>
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Load
+        </button>
       </div>
       <Footer />
     </div>
