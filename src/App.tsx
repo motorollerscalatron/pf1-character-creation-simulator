338a5cceb260da3956018a230e88b454a57f6a22
motoroller.scalatron <motoroller.scalatron75@gmail.com>
2021-03-26 03:09:07 +0900

    [WIP]draggable images

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from "react-router-dom";
-import CounterBoard from "./views/CounterBoard";
+} from 'react-router-dom';
+import CounterBoard from './views/CounterBoard';
+import FormationBoard from './views/FormationBoard';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/formation" component={FormationBoard} />
           <Route path="/" component={CounterBoard} />
           <Route path="*">
             <Redirect to="/" />
